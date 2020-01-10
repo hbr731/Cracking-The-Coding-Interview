63f219e56b02057102a22228ee8e451a28f27bc1
habib1234731 <habibrehman1234731@gmail.com>
2020-01-10 18:01:49 -0500

    implemented basic linkedList functions

--- LinkedList/src/com/javaTwo/LinkedList.java
+++ LinkedList/src/com/javaTwo/LinkedList.java
@@ -1,5 +1,7 @@
 package com.javaTwo;
 
+import java.util.NoSuchElementException;
+
 public class LinkedList<E> {
 
     protected class Node {
@@ -10,4 +12,62 @@ public class LinkedList<E> {
     }
 
     protected Node head;
+
+    public int size() {
+        int sz = 0;
+        for (Node tnd = head; tnd != null; tnd = tnd.next) sz++;
+        return sz;
+    }
+
+    public void addFirst(E elt) {
+        Node nnd = new Node(elt);
+        nnd.next = head;
+        head = nnd;
+    }
+
+    public boolean add (E elt) {
+        Node nnd = new Node(elt);
+
+        if (head == null) {
+            head = nnd;
+            return true;
+        }
+
+        Node tnd = head;
+        while (tnd.next != null) tnd = tnd.next;
+
+        tnd.next = nnd;
+        return true;
+    }
+
+    public E remove() {
+        if (head == null) throw new NoSuchElementException();
+
+        E ret = head.data;
+        head = head.next;
+        return ret;
+    }
+
+    public E remove (int idx) {
+        if (idx < 0) throw new IndexOutOfBoundsException();
+        if (idx == 0) return remove();
+
+        Node tnd = head;
+        int i = 0;
+
+        while (i < idx - 1) {
+            if (tnd != null) tnd = tnd.next;
+            else throw new IndexOutOfBoundsException();
+            i++;
+        }
+
+        E ret;
+        if (tnd.next != null) {
+            ret = tnd.next.data;
+            tnd.next = tnd.next.next;
+        } else throw new IndexOutOfBoundsException();
+
+        return ret;
+    }
+
 }
